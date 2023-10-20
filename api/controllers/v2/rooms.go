c511f4d6d897f38e1480676f3ebfe85513d789d2
amorist <amor90s.zhang@gmail.com>
2023-10-20 13:09:46 +0800

    dgshare

--- api/controllers/v2/rooms.go
+++ api/controllers/v2/rooms.go
@@ -46,64 +46,63 @@ func (ctl *RoomController) Rooms(ctx context.Context, webCtx web.Context, user *
 	}
 
 	var suggests []repo.GalleryRoom
-	if len(rooms) == 0 {
-		suggests, err = ctl.roomRepo.GallerySuggests(ctx, 11)
-		if err != nil {
-			log.Errorf("查询推荐房间列表失败: %v", err)
-			// 注意：这里不返回错误，因为推荐房间列表不是必须的
+	// if len(rooms) == 0 {
+	suggests, err = ctl.roomRepo.GallerySuggests(ctx, 11)
+	if err != nil {
+		log.Errorf("查询推荐房间列表失败: %v", err)
+		// 注意：这里不返回错误，因为推荐房间列表不是必须的
+	}
+
+	cnLocalMode := client.IsCNLocalMode(ctl.conf)
+	suggests = array.Filter(suggests, func(item repo.GalleryRoom, _ int) bool {
+		// 如果启用了国产化模式，则过滤掉 openai 和 Anthropic 的模型
+		if cnLocalMode && item.RoomType == "system" && array.In(item.Vendor, []string{"openai", "Anthropic"}) {
+			return false
 		}
 
-		cnLocalMode := client.IsCNLocalMode(ctl.conf)
-		suggests = array.Filter(suggests, func(item repo.GalleryRoom, _ int) bool {
-			// 如果启用了国产化模式，则过滤掉 openai 和 Anthropic 的模型
-			if cnLocalMode && item.RoomType == "system" && array.In(item.Vendor, []string{"openai", "Anthropic"}) {
-				return false
-			}
+		// 检查模型是否满足条件
+		if !ctl.conf.EnableOpenAI && item.Vendor == "openai" {
+			return false
+		}
 
-			// 检查模型是否满足条件
-			if !ctl.conf.EnableOpenAI && item.Vendor == "openai" {
-				return false
-			}
+		if !ctl.conf.EnableBaiduWXAI && item.Vendor == "文心千帆" {
+			return false
+		}
 
-			if !ctl.conf.EnableBaiduWXAI && item.Vendor == "文心千帆" {
-				return false
-			}
+		if !ctl.conf.EnableDashScopeAI && item.Vendor == "灵积" {
+			return false
+		}
 
-			if !ctl.conf.EnableDashScopeAI && item.Vendor == "灵积" {
-				return false
-			}
+		if !ctl.conf.EnableXFYunAI && item.Vendor == "讯飞星火" {
+			return false
+		}
 
-			if !ctl.conf.EnableXFYunAI && item.Vendor == "讯飞星火" {
-				return false
-			}
+		if !ctl.conf.EnableSenseNovaAI && item.Vendor == "商汤日日新" {
+			return false
+		}
 
-			if !ctl.conf.EnableSenseNovaAI && item.Vendor == "商汤日日新" {
-				return false
-			}
+		if !ctl.conf.EnableTencentAI && item.Vendor == "腾讯" {
+			return false
+		}
 
-			if !ctl.conf.EnableTencentAI && item.Vendor == "腾讯" {
-				return false
-			}
+		if !ctl.conf.EnableAnthropic && item.Vendor == "Anthropic" {
+			return false
+		}
 
-			if !ctl.conf.EnableAnthropic && item.Vendor == "Anthropic" {
-				return false
-			}
-
-			if item.VersionMax == "" && item.VersionMin == "" {
-				return true
-			}
-
-			if item.VersionMin != "" && helper.VersionOlder(client.Version, item.VersionMin) {
-				return false
-			}
-
-			if item.VersionMax != "" && helper.VersionNewer(client.Version, item.VersionMax) {
-				return false
-			}
-
+		if item.VersionMax == "" && item.VersionMin == "" {
 			return true
-		})
-	}
+		}
+
+		if item.VersionMin != "" && helper.VersionOlder(client.Version, item.VersionMin) {
+			return false
+		}
+
+		if item.VersionMax != "" && helper.VersionNewer(client.Version, item.VersionMax) {
+			return false
+		}
+
+		return true
+	})
 
 	return webCtx.JSON(web.M{
 		"data":     rooms,
